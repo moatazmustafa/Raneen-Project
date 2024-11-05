88eda350b76fcad1891d7f19fc5d972bcf2d1e64
moataztest <moatazmustafa123@gmail.com>
2024-11-05 14:14:20 +0200

    teeeest git

---
+++ src/test/java/Regestration.java
@@ -0,0 +1,94 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import io.qameta.allure.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Regestration {
+
+ WebDriver driver;
+
+    @BeforeTest
+    public void prepare ()
+    {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+
+
+    @AfterTest
+    public void shutdown (){
+
+        driver.quit();
+
+    }
+
+    @Description("this script for testing smart store registration")
+    @Severity(SeverityLevel.CRITICAL)
+    @Owner("Moataz Mustafa")
+    @Link(name = "smart store", url = "https://www.raneen.com/ss_zayed/smartstore/home/map")
+    @Issue("https://raneen.atlassian.net/browse/RT-521?atlOrigin=eyJpIjoiODg0YjM1MmU5NjdhNDlmOTk2MTE3MTlhMGY1YzkxYjMiLCJwIjoiaiJ9")
+    @TmsLink("Zephyr Scale")
+    @Epic("Testing Automation")
+    @Feature("Registration")
+    @Story("valid registration")
+    @Test
+    public void open_screen_setup() throws InterruptedException {
+
+        driver.get("https://www.raneen.com/ss_zayed/smartstore/screen/setup/");
+        driver.findElement(By.id("screen_type")).click();
+        {
+            WebElement dropdown = driver.findElement(By.id("screen_type"));
+            dropdown.findElement(By.xpath("//option[. = 'التسجيل']")).click();
+        }
+
+        driver.findElement(By.id("identifier")).sendKeys("motaz registration");
+        driver.findElement(By.id("password")).sendKeys("mm@123456");
+        driver.findElement(By.xpath("//form[@id='screen-setup-form']/div[5]/div/button/span")).click();
+        driver.findElement(By.id("phone")).sendKeys("01555329555");
+        driver.findElement(By.xpath("//form/button")).click();
+        Thread.sleep(3000);
+        driver.switchTo().newWindow(WindowType.TAB);
+        Object[] windowHandles=driver.getWindowHandles().toArray();
+        driver.switchTo().window((String) windowHandles[1]);
+        driver.get("https://www.raneen.com/admin/admin/");
+        driver.findElement(By.id("username")).sendKeys("motaz.mostafa");
+        driver.findElement(By.id("login")).sendKeys("mm@123456");
+        driver.findElement(By.xpath("//form[@id='login-form']/fieldset/div[3]/div/button/span")).click(); //login button
+        Thread.sleep(5000);
+        driver.findElement(By.xpath("//li[@id='menu-ocean-core-ocean']/a")).click();                      // ocean tab
+        driver.findElement(By.xpath("//li[@id='menu-ocean-core-ocean']/div/ul/li[2]/ul/li/div/ul/li[4]/a/span")).click();  //otp tab
+        Thread.sleep(5000);
+        String OTP =driver.findElement(By.xpath("//tbody/tr/td[3]")).getText(); //copy
+        driver.switchTo().window((String) windowHandles[0]);
+        driver.findElement(By.id("otp")).sendKeys(OTP);    //paste otp
+        driver.findElement(By.id("firstname")).sendKeys("moataz"); //firstname
+        driver.findElement(By.id("lastname")).sendKeys("mustafa"); //lastname
+        driver.findElement(By.xpath("//div[2]/div/form/button")).click(); //submit
+        Thread.sleep(3000);
+        driver.switchTo().window((String) windowHandles[1]);   //back to admin
+        driver.findElement(By.xpath("//nav/ul/li[8]/a/span")).click(); //customers
+        Thread.sleep(5000);
+        driver.findElement(By.xpath("//li[8]/div/ul/li/a/span")).click(); //all customer tab
+        Thread.sleep(5000);
+        driver.findElement(By.xpath("//div[2]/div/div[2]/input")).clear();
+        driver.findElement(By.xpath("//div[2]/div/div[2]/input")).sendKeys("01555329555");
+        Thread.sleep(5000);
+        driver.findElement(By.xpath("//div[2]/div/div[2]/button")).click();
+        Thread.sleep(10000);
+        driver.findElement(By.xpath("//td[22]/a")).click();
+        Thread.sleep(5000);
+        driver.findElement(By.xpath("//button[3]/span")).click();
+        Thread.sleep(5000);
+        driver.findElement(By.xpath("//footer/button[2]/span")).click();
+        Thread.sleep(5000);
+    }
+}
---
+++ src/test/java/SS_PaymentMethods_with_storeCreditAndCoupon.java
@@ -0,0 +1,94 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.hamcrest.core.Is;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import javax.swing.*;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+public class SS_PaymentMethods_with_storeCreditAndCoupon {
+
+    WebDriver driver;
+
+
+    @BeforeTest
+    public void prepare() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+
+
+    @AfterTest
+    public void shutdown() {
+
+        driver.quit();
+
+    }
+
+
+    @Test
+    public void open_screen_setup() throws InterruptedException {
+
+        driver.get("https://www.raneen.com/admin/admin/");  //open magento
+        driver.findElement(By.cssSelector("#username")).sendKeys("motaz.mostafa");  //user name
+        driver.findElement(By.cssSelector("#login")).sendKeys("mm@123456");  //password
+        driver.findElement(By.cssSelector("#login-form > fieldset > div.form-actions > div.actions > button")).click();  //sign in
+        Thread.sleep(5000);
+        driver.findElement(By.cssSelector("#menu-magento-catalog-catalog > a")).click();  // catalog
+        driver.findElement(By.cssSelector("#menu-magento-catalog-catalog > div > ul > li.item-inventory.parent.level-1 > div > ul > li.item-catalog-products.level-2 > a")).click();  //products
+        Thread.sleep(20000);
+        driver.findElement(By.xpath("//*[@id=\"fulltext\"]")).clear();  // product search field
+        driver.findElement(By.xpath("//*[@id=\"fulltext\"]")).sendKeys("test33"); //
+        driver.findElement(By.cssSelector("#container > div > div.admin__data-grid-header > div:nth-child(1) > div.data-grid-search-control-wrap > button")).click();  // search
+        driver.findElement(By.cssSelector("#idscheck413176")).click();  // checkbox
+        driver.findElement(By.cssSelector("#container > div > div.admin__data-grid-header > div.admin__data-grid-header-row.row.row-gutter > div.col-xs-2 > div > button")).click();  // mass action
+        driver.findElement(By.cssSelector("#container > div > div.admin__data-grid-header > div.admin__data-grid-header-row.row.row-gutter > div.col-xs-2 > div > div > ul > li._parent > span")).click();  //change status
+        driver.findElement(By.cssSelector("#container > div > div.admin__data-grid-header > div.admin__data-grid-header-row.row.row-gutter > div.col-xs-2 > div > div > ul > li._parent._visible > ul > li:nth-child(1) > span")).click();  //enable
+        driver.switchTo().newWindow(WindowType.TAB);
+        Object[] windowHandles=driver.getWindowHandles().toArray();
+        driver.switchTo().window((String) windowHandles[1]);
+
+        driver.get("https://www.raneen.com/ss_zayed/catalog/product/view/id/413176"); // test product
+        driver.findElement(By.cssSelector("#smart-addtocart-button")).click();  // add t cart
+        driver.findElement(By.cssSelector("#phone-number")).sendKeys("01500008863");  // phone num
+        driver.findElement(By.cssSelector("#html-body > div.modals-wrapper > aside > div.modal-inner-wrap > footer > button")).click();  // add t cart
+        Thread.sleep(5000);
+        driver.switchTo().newWindow(WindowType.TAB);
+        Object[] windowHandles2=driver.getWindowHandles().toArray();
+        driver.switchTo().window((String) windowHandles2[1]);
+        driver.get("https://www.raneen.com/ss_zayed/smartstore/screen/setup");
+        driver.findElement(By.cssSelector("#screen_type")).click();  // screen type
+        driver.findElement(By.cssSelector("#screen_type > option:nth-child(4)")).click();  //checkout screen
+        driver.findElement(By.cssSelector("#identifier")).sendKeys("motaz checkout");  //
+        driver.findElement(By.cssSelector("#password")).sendKeys("mm@123456");  //
+        driver.findElement(By.cssSelector("#screen-setup-form > div.actions-toolbar > div > button")).click();  // setup screen
+        driver.findElement(By.cssSelector("#phone")).sendKeys("01500008863");  //send OTP
+        driver.findElement(By.cssSelector("#smartstore-otp > button")).click();  //send
+        driver.switchTo().window((String) windowHandles2[0]); //magento
+        driver.findElement(By.cssSelector("#menu-ocean-core-ocean > a")).click();  //ocean
+        driver.findElement(By.cssSelector("#menu-ocean-core-ocean > div > ul > li:nth-child(2) > ul > li:nth-child(1) > div > ul > li.item-smartotp.level-2 > a")).click();  //OTPs
+        String OTP =driver.findElement(By.xpath("//tbody/tr/td[3]")).getText(); //copy
+        driver.switchTo().window((String) windowHandles2[1]); //magento
+        Thread.sleep(5000);
+        driver.findElement(By.cssSelector("")).click();  //
+        driver.findElement(By.cssSelector("")).click();  //
+        driver.findElement(By.cssSelector("")).click();  //
+        driver.findElement(By.cssSelector("")).click();  //
+        driver.findElement(By.cssSelector("")).click();  //
+
+
+
+
+
+
+
+    }
+}
