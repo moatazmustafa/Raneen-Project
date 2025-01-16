ff2919a28c0181d0416a7efd601f53113d985b33
Motaz Mostafa Hamam Mohamed <motaz.mostafa@raneen.com.eg>
2025-01-16 18:59:54 +0200

    Hot Fix

--- src/test/java/Tests/RegistrationTest.java
+++ src/test/java/Tests/RegistrationTest.java
@@ -7,13 +7,17 @@ import Utilities.Utility;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WindowType;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class RegistrationTest {
+    private static final Logger log = LoggerFactory.getLogger(RegistrationTest.class);
 
     WebDriver driver;
     SSRegistrationPage SSRegistrationPage;
@@ -24,9 +28,11 @@ public class RegistrationTest {
         // Set up WebDriver
         WebDriverManager.chromedriver().setup();
 
-        // Configure ChromeOptions for Incognito Mode
+        // Configure ChromeOptions for Incognito Mode //
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("--headless");
+        //   options.addArguments("--headless");
+        options.addArguments("--window-size=1920,1080");
+
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--disable-gpu");
@@ -37,7 +43,7 @@ public class RegistrationTest {
         options.addArguments("--disable-cache"); // Disable caching
         driver = new ChromeDriver(options); // Initialize driver with options
 
-        // Clear cookies explicitly (if needed) 
+        // Clear cookies explicitly (if needed)
         driver.manage().deleteAllCookies();
 
         // Initialize Page Objects
@@ -55,62 +61,76 @@ public class RegistrationTest {
 
     @Test
     public void testRegistrationFlow() throws InterruptedException {
-        try {
-            // Step 1: Navigate to Registration Page
-            BasePage.openUrl("https://rstore.raneen.com/ss_zayed/smartstore/screen/setup");
-            SSRegistrationPage.selectScreenType("التسجيل");
-            SSRegistrationPage.enterIdentifier("motaz registration");
-            SSRegistrationPage.enterPassword("mm@123456");
-            Utility.clickingOnElement(driver, By.xpath("/html/body/div[2]/main/div[4]/div/div[2]/form/div[5]/div/button"));
-            String randomPhoneNumber = Utility.generateRandomPhoneNumber("015");
-            SSRegistrationPage.enterPhone(randomPhoneNumber);
-            SSRegistrationPage.generateMyBarcode();
+        log.info("Starting Registration Test");
+        // Step 1: Navigate to Registration Page
+        log.info("Navigating to Registration Page");
+        BasePage.openUrl("https://rstore.raneen.com/ss_zayed/smartstore/screen/setup");
+        log.info("Setup Registration Screen");
+        SSRegistrationPage.selectScreenType("التسجيل");
+        SSRegistrationPage.enterIdentifier("motaz registration");
+        SSRegistrationPage.enterPassword("mm@123456");
+        log.info("Clicking on Submit Button");
+        Utility.clickingOnElement(driver, By.xpath("/html/body/div[2]/main/div[4]/div/div[2]/form/div[5]/div/button"));
+        log.info("Enter Random Phone Number Starts With (015)");
+        String randomPhoneNumber = Utility.generateRandomPhoneNumber("015");
+        SSRegistrationPage.enterPhone(randomPhoneNumber);
+        log.info("Generate Barcode");
+        SSRegistrationPage.generateMyBarcode();
 
-       /*     // Step 2: Open Admin Portal
-            driver.switchTo().newWindow(WindowType.TAB);
-            Object[] windowHandles = driver.getWindowHandles().toArray();
-            driver.switchTo().window((String) windowHandles[1]);
-            driver.get("https://www.raneen.com/admin/admin/");
-            adminPage.login("motaz.mostafa", "mm@123456");
+        // Step 2: Open Admin Portal
+        log.info("Opening Admin Portal");
+        driver.switchTo().newWindow(WindowType.TAB);
+        Object[] windowHandles = driver.getWindowHandles().toArray();
+        driver.switchTo().window((String) windowHandles[1]);
+        driver.get("https://www.raneen.com/admin/admin/");
+        log.info("Logging in to Admin Portal");
+        adminPage.login("motaz.mostafa", "mm@123456");
+        log.info("Navigating to Ocean OTP Tab");
+        adminPage.navigateToOtpTab();
+        log.info("Getting OTP");
+        String otp = adminPage.getOtp();
 
-            adminPage.navigateToOtpTab();
-            String otp = adminPage.getOtp();
+        // Step 3: Complete Registration
+        log.info("Switch To Registration Screen");
+        driver.switchTo().window(driver.getWindowHandles().iterator().next());
+        log.info("Enter OTP");
+        SSRegistrationPage.enterOtp(otp);
+        log.info("Enter First Name");
+        SSRegistrationPage.enterFirstName("moataz");
+        log.info("Enter Last Name");
+        SSRegistrationPage.enterLastName("mustafa");
+        log.info("Click On Submit Registration");
+        SSRegistrationPage.submitRegistration();
 
-            // Step 3: Complete Registration
-            driver.switchTo().window(driver.getWindowHandles().iterator().next());
-            SSRegistrationPage.enterOtp(otp);
-            SSRegistrationPage.enterFirstName("moataz");
-            SSRegistrationPage.enterLastName("mustafa");
-            SSRegistrationPage.submitRegistration();
+        // Step 4: Verify Registration in Admin Portal
+        log.info("Switch To Admin Portal");
+        driver.switchTo().window((String) windowHandles[1]);
+        Thread.sleep(5000);
+        log.info("Navigating To Customers Tab");
+        Utility.clickingOnElement(driver, By.xpath("/html/body/div[1]/nav/ul/li[8]/a"));  //customer tab
+        Thread.sleep(3000);
+        log.info("Navigating To All Customers Tab");
+        Utility.clickingOnElement(driver, By.xpath("/html/body/div[1]/nav/ul/li[8]/div/ul/li[1]/a"));
+        log.info("Clear Search Field");
+        Utility.findWebElement(driver, By.xpath("/html/body/div[2]/main/div[2]/div/div/div[2]/div[2]/div[1]/div[2]/input")).clear();
+        log.info("Enter The Random Phone Number");
+        Utility.sendData(driver, By.xpath("/html/body/div[2]/main/div[2]/div/div/div[2]/div[2]/div[1]/div[2]/input"), randomPhoneNumber);
+        Thread.sleep(15000);
+        log.info("Click On Search Button");
+        Utility.clickingOnElement(driver, By.xpath("//div[2]/div/div[2]/button")); // Search
+        Thread.sleep(5000);
 
-            // Step 4: Verify Registration in Admin Portal
-            driver.switchTo().window((String) windowHandles[1]);
-            Thread.sleep(5000);
-
-            Utility.clickingOnElement(driver, By.xpath("/html/body/div[1]/nav/ul/li[8]/a"));  //customer tab
-            Thread.sleep(3000);
-            Utility.clickingOnElement(driver, By.xpath("/html/body/div[1]/nav/ul/li[8]/div/ul/li[1]/a"));
-
-            Utility.findWebElement(driver, By.xpath("/html/body/div[2]/main/div[2]/div/div/div[2]/div[2]/div[1]/div[2]/input")).clear();
-
-            Utility.sendData(driver, By.xpath("/html/body/div[2]/main/div[2]/div/div/div[2]/div[2]/div[1]/div[2]/input"), randomPhoneNumber);
-            Thread.sleep(15000);
-            Utility.clickingOnElement(driver, By.xpath("//div[2]/div/div[2]/button")); // Search
-            Thread.sleep(5000);
-
-            // Open Customer Details
-            Utility.findWebElement(driver, By.xpath("//td[22]/a"));
-            Thread.sleep(10000);
-            Utility.clickingOnElement(driver, By.xpath("/html/body/div[2]/main/div[2]/div/div/div[2]/div[4]/table/tbody/tr[2]/td[22]/a")); // Open customer details
-            Thread.sleep(3000);
-            Utility.clickingOnElement(driver, By.xpath("//button[3]/span")); // Action
-            Thread.sleep(3000);
-            Utility.clickingOnElement(driver, By.xpath("//footer/button[2]/span")); // Save or confirm
-*/
-        } catch (Exception e) {
-            e.printStackTrace();
-            Utility.takeScreenShot(driver, "TestFailure");
-            throw e;
-        }
+        // Open Customer Details
+        Utility.findWebElement(driver, By.xpath("//td[22]/a"));
+        Thread.sleep(10000);
+        log.info("Open Customer Details");
+        Utility.clickingOnElement(driver, By.xpath("/html/body/div[2]/main/div[2]/div/div/div[2]/div[4]/table/tbody/tr[2]/td[22]/a")); // Open customer details
+        Thread.sleep(3000);
+        log.info("Delete Customer");
+        Utility.clickingOnElement(driver, By.xpath("//button[3]/span")); // Action
+        Thread.sleep(3000);
+        log.info("Save");
+        Utility.clickingOnElement(driver, By.xpath("//footer/button[2]/span")); // Save or confirm
+        log.info("Test Completed Successfully");
     }
 }
