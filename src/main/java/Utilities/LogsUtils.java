644e059eb0cb24e6e34ef69f7f733050d1741abf
moataztest <moatazmustafa123@gmail.com>
2025-02-13 13:53:16 +0200

    2

--- src/main/java/Utilities/LogsUtils.java
+++ src/main/java/Utilities/LogsUtils.java
@@ -2,41 +2,43 @@ package Utilities;
 
 import io.qameta.allure.Allure;
 import org.apache.logging.log4j.LogManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class LogsUtils {
     public static String LOGS_PATH = "test-outputs/Logs";
+    private static final org.apache.logging.log4j.Logger log = LogManager.getLogger(LogsUtils.class);
 
     public static void trace(String message) {
-        LogManager.getLogger(Thread.currentThread().getStackTrace()[2].toString())
-                .trace(message);
+       log.trace(message);
         Allure.addAttachment("logs", message);
 
     }
 
     public static void debug(String message) {
-        LogManager.getLogger(Thread.currentThread().getStackTrace()[2].toString())
-                .debug(message);
+        log.debug(message);
+
         Allure.addAttachment("logs", message);
 
     }
 
     public static void info(String message) {
-        LogManager.getLogger(Thread.currentThread().getStackTrace()[2].toString())
-                .info(message);
+        log.info(message);
+
         Allure.addAttachment("logs", message);
 
     }
 
     public static void warn(String message) {
-        LogManager.getLogger(Thread.currentThread().getStackTrace()[2].toString())
-                .warn(message);
+        log.warn(message);
+
         Allure.addAttachment("logs", message);
 
     }
 
     public static void error(String message) {
-        LogManager.getLogger(Thread.currentThread().getStackTrace()[2].toString())
-                .error(message);
+        log.error(message);
+
         Allure.addAttachment("logs", message);
 
     }
