f2c6bd40d1d3e1004ecb96f2dc36bedc30beda5f
moataz-mustafa <moatazmustafa123@gmail.com>
2025-04-07 13:49:29 +0200

    headless

--- src/test/java/Tests/WsOrderCycleTest.java
+++ src/test/java/Tests/WsOrderCycleTest.java
@@ -34,7 +34,7 @@ public class WsOrderCycleTest {
 
         // Configure ChromeOptions for Incognito Mode //
         ChromeOptions options = new ChromeOptions();
-      //  options.addArguments("--headless");
+        options.addArguments("--headless");
         options.addArguments("--window-size=1920,1080");
 
         options.addArguments("--no-sandbox");
