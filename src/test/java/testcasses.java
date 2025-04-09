6f53bbdb689e05337207811ca2b612552c00ccf4
moataz-mustafa <moatazmustafa123@gmail.com>
2025-04-09 17:01:00 +0200

    Update testcasses.java

--- src/test/java/testcasses.java
+++ src/test/java/testcasses.java
@@ -664,7 +664,7 @@ public class testcasses {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        driver.findElement(By.id("wallet-phone-input")).sendKeys("01555888986");
+        driver.findElement(By.id("wallet-phone-input")).sendKeys("*********");
         try {
             Thread.sleep(2000); // Wait for 2 seconds
         } catch (InterruptedException e) {
