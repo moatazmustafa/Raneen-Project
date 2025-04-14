a8e71e2dd62a2239b91e6e9df21036947cac55a1
moataz-mustafa <moatazmustafa123@gmail.com>
2025-04-14 18:00:25 +0200

    orderDetails

--- src/test/java/Tests/ApiOrderDetailsTest.java
+++ src/test/java/Tests/ApiOrderDetailsTest.java
@@ -2,20 +2,28 @@ package Tests;
 
 import BaseApi.BaseApiTest;
 import io.restassured.response.Response;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.Test;
 
 import static org.hamcrest.Matchers.*;
 
 public class ApiOrderDetailsTest extends BaseApiTest {
+    private static final Logger log = LoggerFactory.getLogger(ApiOrderDetailsTest.class);
 
-    private final String validToken = "56ie5hmwbni9b02wppiqi27a3qjejx8m";
-    private final String invalidToken = "invalid_token";
-    private final String validIncrementId = "2003262640";
-    private final String invalidIncrementId = "123456";
+     String validToken = "56ie5hmwbni9b02wppiqi27a3qjejx8m";
+     String invalidToken = "invalid_token";
+     String validIncrementId = "2003262640";
+     String invalidIncrementId = "123456";
 
     @Test(priority = 1)
     public void testOrderDetails_Valid_AR() {
-        Response response = sendOrderDetailsRequest(validToken, validIncrementId, 2);
+        Response response  = withDefaultHeaders()
+                .queryParam("storeId", 2) //AR storeId
+                .queryParam("customerToken", validToken)
+                .queryParam("incrementId", validIncrementId)
+                .get("/ar/mobileapi/v2_customer/orderdetails");
+
         response.then().statusCode(200)
                 .body("success", equalTo(true))
                 .body("customerEmail", equalTo("motaz.mostafa@raneen.com"))
@@ -27,7 +35,12 @@ public class ApiOrderDetailsTest extends BaseApiTest {
 
     @Test(priority = 2)
     public void testOrderDetails_Valid_EN() {
-        Response response = sendOrderDetailsRequest(validToken, validIncrementId, 4);
+        Response response  = withDefaultHeaders()
+                .queryParam("storeId", 4)
+                .queryParam("customerToken", validToken)
+                .queryParam("incrementId", validIncrementId)
+                .get("/en/mobileapi/v2_customer/orderdetails");
+
         response.then().statusCode(200)
                 .body("success", equalTo(true))
                 .body("customerEmail", equalTo("motaz.mostafa@raneen.com"))
@@ -39,7 +52,12 @@ public class ApiOrderDetailsTest extends BaseApiTest {
 
     @Test(priority = 3)
     public void testOrderDetails_InvalidToken_AR() {
-        Response response = sendOrderDetailsRequest(invalidToken, validIncrementId, 2);
+        Response response  = withDefaultHeaders()
+                .queryParam("storeId", 2)
+                .queryParam("customerToken", invalidToken)
+                .queryParam("incrementId", validIncrementId)
+                .get("/ar/mobileapi/v2_customer/orderdetails");
+
         response.then().statusCode(200)
                 .body("success", equalTo(false))
                 .body("message", containsString("كعميل تطلبه غير موجود"))
@@ -48,36 +66,44 @@ public class ApiOrderDetailsTest extends BaseApiTest {
 
     @Test(priority = 4)
     public void testOrderDetails_InvalidToken_EN() {
-        Response response = sendOrderDetailsRequest(invalidToken, validIncrementId, 4);
+        Response response  = withDefaultHeaders()
+                .queryParam("storeId", 4)
+                .queryParam("customerToken", invalidToken)
+                .queryParam("incrementId", validIncrementId)
+                .get("/en/mobileapi/v2_customer/orderdetails");
+
         response.then().statusCode(200)
                 .body("success", equalTo(false))
-                .body("message", containsString("customer you are requesting does not exist"))
+                .body("message", containsString("As customer you are requesting does not exist, so you need to logout."))
+                .body("shippingMethod", containsString("No shipping information available"))
                 .body("otherError", equalTo("customerNotExist"));
     }
 
     @Test(priority = 5)
     public void testOrderDetails_InvalidIncrementId_AR() {
-        Response response = sendOrderDetailsRequest(validToken, invalidIncrementId, 2);
+        Response response  = withDefaultHeaders()
+                .queryParam("storeId", 2)
+                .queryParam("customerToken", validToken)
+                .queryParam("incrementId", invalidIncrementId)
+                .get("/ar/mobileapi/v2_customer/orderdetails");
+
         response.then().statusCode(200)
                 .body("success", equalTo(false))
-                .body("message", containsString("طلب غير صحيح"))
+                .body("message", containsString("طلب غير صحيح."))
                 .body("shippingMethod", containsString("لا تتوفر معلومات الشحن"));
     }
 
     @Test(priority = 6)
     public void testOrderDetails_InvalidIncrementId_EN() {
-        Response response = sendOrderDetailsRequest(validToken, invalidIncrementId, 4);
+        Response response  = withDefaultHeaders()
+                .queryParam("storeId", 4)
+                .queryParam("customerToken", validToken)
+                .queryParam("incrementId", invalidIncrementId)
+                .get("/mobileapi/v2_customer/orderdetails");
         response.then().statusCode(200)
                 .body("success", equalTo(false))
                 .body("message", containsString("Invalid Order"))
                 .body("shippingMethod", containsString("No shipping information available"));
     }
 
-    private Response sendOrderDetailsRequest(String token, String incrementId, int storeId) {
-        return withDefaultHeaders()
-                .queryParam("storeId", storeId)
-                .queryParam("customerToken", token)
-                .queryParam("incrementId", incrementId)
-                .get("/mobileapi/v2_customer/orderdetails");
-    }
 }
